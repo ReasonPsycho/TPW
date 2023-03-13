21c20d9bef70495eb28652b5b51df54cae659eb7
ReasonPsycho <54778479+ReasonPsycho@users.noreply.github.com>
2023-03-13 13:20:19 +0100

    Initial commit - Etap 0

---
+++ TPW/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace TPW
+{
+    public class Calculator
+    {
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+    
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+    
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+    
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+    }
+}
