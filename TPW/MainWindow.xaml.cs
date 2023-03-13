e7aca8ed48f6c8b27465cff8fa13273dc8df6835
ReasonPsycho <54778479+ReasonPsycho@users.noreply.github.com>
2023-03-13 13:20:47 +0100

    Ininital commit the rest of files

---
+++ TPW/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace TPW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+  
+    
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Calculator.Add(float.Parse(a.Text) , float.Parse(b.Text)).ToString()) ;
+        }
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( Calculator.Divide(float.Parse(a.Text) , float.Parse(b.Text)).ToString()) ;
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( Calculator.Subtract(float.Parse(a.Text) , float.Parse(b.Text)).ToString()) ;
+        }
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( Calculator.Multiply(float.Parse(a.Text) , float.Parse(b.Text)).ToString()) ;
+        }
+    }
+
+   
+}
---
+++ UnitTests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using TPW;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Add()
+        {
+            Assert.True(Calculator.Add(6, 2) == 6 + 2);
+        }
+        
+        [Test]
+        public void Divide()
+        {
+            Assert.True(Calculator.Divide(2, 2) == 2 / 2);
+        }
+        
+        [Test]
+        public void Subtract()
+        {
+            Assert.True(Calculator.Subtract(6, 2) == 6 - 2);
+        }
+        
+        [Test]
+        public void Multiply()
+        {
+            Assert.True(Calculator.Multiply(6, 2) == 6 * 2);
+        }
+    }
+}
